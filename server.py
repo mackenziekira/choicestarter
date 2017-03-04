9c49886d0f9d84857813ae836cdb79066b4cc901
Mackenzie Dolginow <mackenziekira@gmail.com>
2017-03-04 10:06:10 -0800

    skeleton for server file

---
+++ server.py
@@ -0,0 +1,42 @@
+from jinja2 import StrictUndefined
+from flask import Flask, jsonify, render_template, redirect, request, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+
+
+
+cache = SimpleCache()
+app = Flask(__name__)
+
+# Raises an error if you use undefined Jinja variable.
+app.jinja_env.undefined = StrictUndefined
+
+
+
+@app.route('/')
+def index():
+    """Homepage."""
+
+    return render_template("homepage.html")
+
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    # app.debug = True
+    # app.config['SQLALCHEMY_ECHO'] = True
+
+    PORT = int(os.environ.get("PORT", 5000))
+    DEBUG = "NO_DEBUG" not in os.environ
+    SECRET_KEY = os.environ.get("FLASK_SECRET_KEY", "asdf9k$")
+
+    # Required to use Flask sessions and the debug toolbar
+    app.secret_key = SECRET_KEY
+
+
+    app.jinja_env.auto_reload = True
+    connect_to_db(app, os.environ.get("DATABASE_URL"))
+
+    # Use the DebugToolbar
+    # DebugToolbarExtension(app)
+
+    app.run(port=PORT, debug=DEBUG)
